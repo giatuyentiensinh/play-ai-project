d4de0dfaf84f97767fa668f46699a16d97f33e20
Tuyen Nguyen Gia <giatuyentiensinh@gmail.com>
2015-11-30 03:01:40 +0700

    Add global Play

---
+++ app/Global.java
@@ -0,0 +1,13 @@
+import play.Application;
+import play.GlobalSettings;
+
+public class Global extends GlobalSettings {
+
+	@Override
+	public void onStart(Application app) {
+		super.onStart(app);
+		// chèn code load data vào đây
+		
+	}
+
+}
