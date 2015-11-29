c9e2624b5dc937d79660031d97a71bf79a8d339c
tuananh <bta.ican@gmail.com>
2015-11-30 02:08:30 +0700

    test đồ thị

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,22 @@
 "use strict";
 angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope, $http) {
 
-	// $http.get('/data')
-	// 	.success(function(data) {
-	// 		var k = [];
-	// 		var RMSE = [];
+	$http.get('/data')
+		.success(function(data) {
+			var k = [];
+			var RMSE = [];
 
-	// 		for (var i = 0; i < data.length; i++) {
-	// 			k.push(data[i].k);
-	// 			RMSE.push(data[i].RMSE);
-	// 		};
+			for (var i = 0; i < data.length; i++) {
+				k.push(data[i].k);
+				RMSE.push(data[i].RMSE);
+			};
 
-	// 		$scope.labels = k;
-	// 		$scope.data = RMSE;
-	// 	})
-	// 	.error(function(resp) {
-	// 		console.log('error server');
-	// 	});
+			$scope.labels = k;
+			$scope.data = RMSE;
+		})
+		.error(function(resp) {
+			console.log('error server');
+		});
 
 	$scope.submit = function() {
 		console.log($scope.film);
@@ -30,11 +30,11 @@ angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope, $ht
 		console.log("submit");
 	};
 
-	$http.get('/tuyen')
-		.success(function(data) {
-			$scope.labels = data.RMSE;
-			$scope.data = [data.k, data.k];
-		});
+	// $http.get('/tuyen')
+	// 	.success(function(data) {
+	// 		$scope.labels = data.RMSE;
+	// 		$scope.data = [data.k, data.k];
+	// 	});
 
   // $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   // $scope.series = ['Series A'];
