dc986b4b6864d8ddae2fb2a9efc845668783a16a
Tuyen Nguyen Gia <giatuyentiensinh@gmail.com>
2015-11-30 01:45:33 +0700

    add even submit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -18,13 +18,30 @@ angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope, $ht
 	// 		console.log('error server');
 	// 	});
 
+	$scope.submit = function() {
+		console.log($scope.film);
+		$http.post('/handerfilm', {
+			filmName: $scope.film
+		}).success(function(data) {
+			console.log(data);
+		}).error(function(res) {
+			console.log(res);
+		});
+		console.log("submit");
+	};
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+	$http.get('/tuyen')
+		.success(function(data) {
+			$scope.labels = data.RMSE;
+			$scope.data = [data.k, data.k];
+		});
+
+  // $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  // $scope.series = ['Series A'];
+  // $scope.data = [
+  //   [65, 59, 80, 81, 56, 55, 40],
+  //   [28, 48, 40, 19, 86, 27, 90]
+  // ];
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
